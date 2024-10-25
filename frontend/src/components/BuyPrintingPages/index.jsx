d119092ce33f5c7fe1b91c36650466a2ca62882c
trlocne <tr.locne204@gmail.com>
2024-10-25 17:18:25 +0700

    fix css BuyPrintingPages

--- frontend/src/components/BuyPrintingPages/index.jsx
+++ frontend/src/components/BuyPrintingPages/index.jsx
@@ -3,19 +3,23 @@ import './stylemodule.css'
 
 export const BuyPrintingPages = () => {
   
-  
   // Tính tổng tiền dựa trên số trang in
   function updateTotal() {
     let tongTienGiayEl = document.getElementById('tong-tien-giay');
     let giamGiaEl = document.getElementById('giam-gia');
     let tongThanhToanEl = document.getElementById('tong-thanh-toan');
+    let inputSoTrang = document.getElementById('so-trang');
     var soTrang =  inputSoTrang?.value; // Mặc định là 100 nếu không nhập
     var giaTrang = 1000; // 1 tờ = 1000 vnd
     var tongTienGiay = soTrang * giaTrang;
     var giamGia = 9000; // Giảm giá cố định là 9.000 vnd
 
     tongTienGiayEl.textContent = tongTienGiay ? `${tongTienGiay} vnd` : '0 vnd';
+    console.log(tongTienGiay)
+    giamGiaEl.textContent = giamGia ? `${giamGia} vnd` : '0 vnd';
+    console.log(giamGia)
     tongThanhToanEl.textContent = (tongTienGiay - giamGia) ? `${tongTienGiay - giamGia} vnd` : '0 vnd';
+    console.log(tongTienGiay - giamGia)
   }
 
   // Lắng nghe sự kiện khi chọn số trang in từ các nút
@@ -40,10 +44,10 @@ export const BuyPrintingPages = () => {
       <div className="buying">
         <div class="form-group">
           <p className="so-trang">Số trang in cần mua</p>
-          <input type="number" id="so-trang" placeholder="0 tờ" max="100" defaultValue="0" onInput={updateTotal}></input>
+          <input type="number" id="so-trang" placeholder="0 tờ" defaultValue="0" onInput={updateTotal} className="text-center text-[20px] font-bold text-[#515151] outline-none"></input>
           <div className="have-section">
-            <p className= "text having">Số trang in đang có</p>
-            <p className="number text">100 tờ</p>
+            <p className= "text having font-normal">Số trang in đang có</p>
+            <p className="number text font-normal">100 tờ</p>
           </div>
         </div>
         <div class="form-group">
@@ -56,13 +60,13 @@ export const BuyPrintingPages = () => {
         </div>
           <div class="form-group">
           <p className="ma-giam-gia">Mã giảm giá</p>
-          <input type="text" id="ma-giam-gia" placeholder="Nhập mã giảm giá"></input>
+          <input type="text" id="ma-giam-gia" placeholder="Nhập mã giảm giá" className="outline-none"></input>
         </div>
 
         <div class="summary">
           <p>Tổng tiền giấy: <span id="tong-tien-giay">0 vnd</span></p>
           <p>Giảm giá: <span id="giam-gia">0 vnd</span></p>
-          <p>Tổng tiền thanh toán: <span id="tong-thanh-toan">0 vnd</span></p>
+          <p className="font-bold">Tổng tiền thanh toán: <span id="tong-thanh-toan" className="font-bold">0 vnd</span></p>
         </div>
         <button class="btn-thanh-toan">Thanh toán</button>
       </div>
