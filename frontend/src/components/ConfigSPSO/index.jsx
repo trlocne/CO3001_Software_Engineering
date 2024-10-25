258cac9c8bec7fb1019feda59a62b2a18a07444a
rabbit-204 <phongthanhlk18@gmail.com>
2024-10-25 23:12:13 +0700

    task1

--- frontend/src/components/ConfigSPSO/index.jsx
+++ frontend/src/components/ConfigSPSO/index.jsx
@@ -1,13 +1,16 @@
-import React, { useRef, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import classes from './style.module.css';
 import deletePromote from '../../assets/deletePromote.svg'
 import calendar from '../../assets/calendar.svg'
-import filterimage from '../../assets/filterimage.svg'
+// import filterimage from '../../assets/filterimage.svg'
 import arrow from '../../assets/arrow.svg'
-import arrowdown from '../../assets/arrowdown.svg'
+import arrowup from '../../assets/arrowup.svg'
 // import './styleModule.css';
+import Swal from 'sweetalert2/dist/sweetalert2.js'
+import 'sweetalert2/src/sweetalert2.scss'
 const listPromotes = [
   {
+    id: 1,
     term: "HK231",
     startDate: "22/01/2023",
     endDate: "28/01/2023",
@@ -16,6 +19,7 @@ const listPromotes = [
     type: "All"
   },
   {
+    id: 2,
     term: "HK231",
     startDate: "22/01/2023",
     endDate: "28/01/2023",
@@ -24,6 +28,7 @@ const listPromotes = [
     type: "All"
   },
   {
+    id: 3,
     term: "HK231",
     startDate: "22/01/2023",
     endDate: "28/01/2023",
@@ -32,6 +37,7 @@ const listPromotes = [
     type: "All"
   },
   {
+    id: 4,
     term: "HK231",
     startDate: "22/01/2023",
     endDate: "28/01/2023",
@@ -40,6 +46,7 @@ const listPromotes = [
     type: "All"
   },
   {
+    id: 5,
     term: "HK231",
     startDate: "22/01/2023",
     endDate: "28/01/2023",
@@ -48,6 +55,7 @@ const listPromotes = [
     type: "All"
   },
   {
+    id: 6,
     term: "HK231",
     startDate: "22/01/2023",
     endDate: "28/01/2023",
@@ -56,6 +64,7 @@ const listPromotes = [
     type: "All"
   },
   {
+    id: 7,
     term: "HK231",
     startDate: "22/01/2023",
     endDate: "28/01/2023",
@@ -64,6 +73,7 @@ const listPromotes = [
     type: "All"
   },
   {
+    id: 8,
     term: "HK231",
     startDate: "22/01/2023",
     endDate: "28/01/2023",
@@ -72,6 +82,7 @@ const listPromotes = [
     type: "All"
   },
   {
+    id: 9,
     term: "HK231",
     startDate: "22/01/2023",
     endDate: "28/01/2023",
@@ -80,6 +91,7 @@ const listPromotes = [
     type: "All"
   },
   {
+    id: 10,
     term: "HK231",
     startDate: "22/01/2023",
     endDate: "28/01/2023",
@@ -88,6 +100,7 @@ const listPromotes = [
     type: "All"
   },
   {
+    id: 11,
     term: "HK231",
     startDate: "22/01/2023",
     endDate: "28/01/2023",
@@ -96,6 +109,8 @@ const listPromotes = [
     type: "All"
   }
 ];
+
+
 const typefile = [
   "PDF", "WORD", "JFIF", "TIFF", "EPS"
 ];
@@ -107,29 +122,130 @@ export const ConfigSPSO = () => {
   const [typePromote, setTypePromote] = useState("");
   const [selectHK, setSelectHK] = useState("All");
   const [upLoad, setupLoad] = useState(100);
-  const [typeFile, setTypeFile] = useState(dbTypeFile);
-  const [checkDD,setCheckDD] =useState(false);
-  const [arroww,setArroww] =useState({arrow});
-  const [listPromote,setListPromote]= useState(listPromotes);
+  const [typeFiles, setTypeFiles] = useState(dbTypeFile);
+  const [checkDD, setCheckDD] = useState(false);
+  const [arroww, setArroww] = useState(arrow);
+  const [listPromote, setListPromote] = useState(listPromotes);
+  // const [reload, setReload] = useState(false);
   const inputRefs = useRef([]);
+  // const refDD = useRef(null);
+
+  // useEffect(() => {
+  //   fetch("http://localhost:3000/listPromote")
+  //     .then(res => res.json())
+  //     .then(data => {
+  //       setListPromote(data);
+  //       console.log(data);
+  //     }
+  //     )
+  //   fetch("http://localhost:3000/configFile")
+  //     .then(res => res.json())
+  //     .then(data => {
+  //       setupLoad(data[0].sizefile);
+  //       setTypeFiles(data[0].typeFile);
+  //       // console.log(data);
+  //     })
+  // }, [reload])
 
   const submitPromote = (e) => {
     e.preventDefault();
-    const newPromote ={
+    const newPromote = {
       term: e.target[0].value,
+      quantity: e.target[1].value,
       startDate: e.target[2].value,
       endDate: e.target[3].value,
-      quantity: e.target[1].value,
       status: "Hoạt động",
       type: e.target[4].selectedOptions[0].value
     }
-    setListPromote([...listPromote,newPromote]);
+    // fetch("http://localhost:3000/listPromote", {
+    //   method: "POST",
+    //   headers: {
+    //     Accept: "application/json",
+    //     "Content-Type": "application/json"
+    //   },
+    //   body: JSON.stringify(newPromote)
+
+    // })
+    //   .then(res => res.json())
+    //   .then(() => {
+    //     setReload(!reload);
+    //     Swal.fire({
+    //       // position: "top-end",
+    //       icon: "success",
+    //       title: "Tạo khuyến mãi thành công",
+    //       showConfirmButton: false,
+    //       timer: 3000
+    //     });
+    //   })
+    setListPromote([...listPromote, newPromote]);
+    Swal.fire({
+      // position: "top-end",
+      icon: "success",
+      title: "Tạo khuyến mãi thành công",
+      showConfirmButton: false,
+      timer: 3000
+    });
     inputRefs.current.forEach(ref => ref.value = '');
     // console.log(listPromote);
   };
+  const handleDeletePromote = (id) => {
+    Swal.fire({
+      title: "Bạn chắc chắn muốn xóa khuyến mãi này?",
+      text: "Bạn sẽ không thể hoàn tác bước này!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Vẫn xóa!",
+      cancelButtonText: "Hủy"
+    }).then((result) => {
+      if (result.isConfirmed) {
+        // fetch(`http://localhost:3000/listPromote/${id}`, {
+        //   method: "DELETE"
+        // })
+        //   .then(res => res.json())
+        //   .then(() => {
+        //     setReload(!reload);
+        //   })
+        // Hàm xóa khuyến mãi
+        // 
+        setListPromote(listPromote.filter(item => item.id !== id));
+        Swal.fire({
+          title: "Deleted!",
+          text: "Bạn đã xóa thành công.",
+          showConfirmButton: false,
+          icon: "success",
+          timer: 3000
+        });
+      }
+    });
+
+  }
   const submitConfig = (e) => {
     e.preventDefault();
-
+    const newConfigFile = {
+      sizefile: e.target[0].selectedOptions[0].value,
+      typeFile: typeFiles
+    }
+    // fetch("http://localhost:3000/configFile/1", {
+    //   method: "PATCH",
+    //   headers: {
+    //     Accept: "application/json",
+    //     "Content-Type": "application/json"
+    //   },
+    //   body: JSON.stringify(newConfigFile)
+    // })
+    //   .then(res => res.json())
+    //   .then(() => {
+    //     setReload(!reload);
+    //     Swal.fire({
+    //       // position: "top-end",
+    //       icon: "success",
+    //       title: "Chỉnh cấu hình thành công",
+    //       showConfirmButton: false,
+    //       timer: 3000
+    //     });
+    //   })
   };
   const handleFilter = (e) => {
     setSelectHK(e.target.selectedOptions[0].value);
@@ -141,23 +257,18 @@ export const ConfigSPSO = () => {
     // console.log(e.target.selectedOptions[0].value);
     setupLoad(e.target.selectedOptions[0].value);
   };
-  // const handleTypeFile = (e) => {
-  //   setTypeFile(Array.from(e.target.selectedOptions, option => option.value));
-  // };
-  // const createPromote =(e)=>{
-
-  // }
-  const addTF = (e)=>{
+  const addTF = (e) => {
     // console.log(e);
-    setTypeFile([...typeFile,e.item]);
+    setTypeFiles([...typeFiles, e.item]);
   }
-  const delTF = (e)=>{
+  const delTF = (e) => {
     // console.log(e);
-    setTypeFile(typeFile.filter(tf=>tf !== e.item));
+    setTypeFiles(typeFiles.filter(tf => tf !== e.item));
   }
-  const handledd=()=>{
-    setArroww(arroww===arrow?arrowdown:arrow);
+  const handledd = () => {
+    setArroww(arroww === arrowup ? arrow : arrowup);
     setCheckDD(!checkDD);
+    // document.removeEventListener('mousedown', handleClickOutside);
   }
   // console.log(typeFile);
   return (
@@ -168,13 +279,7 @@ export const ConfigSPSO = () => {
       </div>
       <div className={classes.search}>
         <div className={classes.search__filter}>
-          
-          <select value={selectHK} onChange={handleFilter} className={classes.filter_HK}>
-            <option value="All"><img src={filterimage} />All</option>
-            <option value="HK231">HK231</option>
-            <option value="HK232">HK232</option>
-            <option value="HK233">HK233</option>
-          </select>
+
           <select value={selectHK} onChange={handleFilter} className={classes.filter_HK}>
             <option value="All">All</option>
             <option value="HK231">HK231</option>
@@ -182,7 +287,7 @@ export const ConfigSPSO = () => {
             <option value="HK233">HK233</option>
           </select>
         </div>
-        <input className={classes.search__search} placeholder='Search for Library...' />
+        {/* <input className={classes.search__search} placeholder='Search for Library...' /> */}
       </div>
       <div className={classes.table}>
         <table>
@@ -196,17 +301,17 @@ export const ConfigSPSO = () => {
             <th className={classes.hidden_th}></th>
           </thead>
           <tbody style={{ height: "240px", overflowY: "auto" }}>
-            {listPromote.map((item, index) => {
+            {listPromote.map((item) => {
               if (selectHK === "All" || item.term === selectHK) {
                 return (
-                  <tr key={index}>
+                  <tr key={item.id}>
                     <td>{item.term}</td>
                     <td>{item.startDate}</td>
                     <td>{item.endDate}</td>
                     <td>{item.quantity}</td>
                     <td>{item.status}</td>
                     <td>{item.type}</td>
-                    <td><img className={classes.hidden_td} src={deletePromote} /></td>
+                    <td><img className={classes.hidden_td} src={deletePromote} onClick={() => handleDeletePromote(item.id)} /></td>
                   </tr>)
               }
             }
@@ -219,27 +324,27 @@ export const ConfigSPSO = () => {
           <div className={classes.block_1}>
             <div className={classes.block_item}>
               <label htmlFor="term">Học kì</label>
-              <input ref={(el) => inputRefs.current[0] = el} className={classes.item_box} type="text" />
+              <input ref={(el) => inputRefs.current[0] = el} className={`${classes.item_box} bg-[#fff]`} type="text" required />
             </div>
             <div className={classes.block_item}>
               <label htmlFor="quantity">Số trang cấp</label>
-              <input ref={(el) => inputRefs.current[1] = el}  className={classes.item_box} type="text" />
+              <input ref={(el) => inputRefs.current[1] = el} className={classes.item_box} type="text" required />
             </div>
           </div>
           <div className={classes.block_item}>
             <label className={classes.label_date} htmlFor="startDate"><img src={calendar} />Thời gian cấp</label>
-            <input ref={(el) => inputRefs.current[2] = el}  className={`${classes.item_box} ${classes.input_date}`} type="date" />
+            <input ref={(el) => inputRefs.current[2] = el} className={`${classes.item_box} ${classes.input_date}`} type="date" required />
           </div>
         </div>
         <div className={`${classes.fP__block} ${classes.fp__block2}`}>
           <div className={classes.block_item}>
             <label className={classes.label_date} htmlFor="endDate"><img src={calendar} />Thời gian kết thúc</label>
-            <input ref={(el) => inputRefs.current[3] = el}  className={`${classes.item_box} ${classes.input_date}`} type="date" />
+            <input ref={(el) => inputRefs.current[3] = el} className={`${classes.item_box} ${classes.input_date}`} type="date" required />
           </div>
           <div className={classes.block_2}>
             <div className={classes.block_item}>
               <label htmlFor="type">Mã cấp</label>
-              <select ref={(el) => inputRefs.current[4] = el}  className={classes.item_box} value={typePromote} onChange={handleTypePromote}>
+              <select ref={(el) => inputRefs.current[4] = el} className={classes.item_box} value={typePromote} onChange={handleTypePromote}>
                 <option value="All">All</option>
                 <option value="Limted">Limited</option>
               </select>
@@ -262,24 +367,24 @@ export const ConfigSPSO = () => {
           <div className={classes.configFile}>
             <div className={classes.cf_dislay}>
               <div className={classes.cf_content}>
-                {typeFile.map((item, index) => (
-                  <div key={index} onClick={()=>delTF({item})} className={classes.cf_item}>
+                {typeFiles.map((item, index) => (
+                  <div key={index} onClick={() => delTF({ item })} className={classes.cf_item}>
                     <div className={classes.item_content}>{item}</div>
                   </div>
                 ))}
               </div>
-              <div className={`${classes.cf_arrow} ${checkDD? classes.cf_arrow_act : classes.cf_arrow_disact}`}><img onClick={handledd} src={arroww} /></div>
+              <div className={`${classes.cf_arrow}  `}><img onClick={handledd} src={arroww} /></div>
             </div>
-              <div className={`${classes.cf_dd} ${checkDD? classes.appear : classes.disappear}`}>
+            <div className={`${classes.cf_dd} ${checkDD ? classes.appear : classes.disappear}`}>
               <div className={classes.cf_contenthd}>
-                {(typefile.filter(item => !new Set(typeFile).has(item))).map((item, index) => (
-                <div key={index} onClick={()=>addTF({item})}  className={classes.cf_itemhd}>
-                  <div className={classes.itemhd_content}>{item}</div>
-                </div>
-              ))}
+                {(typefile.filter(item => !new Set(typeFiles).has(item))).map((item, index) => (
+                  <div key={index} onClick={() => addTF({ item })} className={classes.cf_itemhd}>
+                    <div className={classes.itemhd_content}>{item}</div>
+                  </div>
+                ))}
               </div>
             </div>
-            
+
           </div>
         </div>
         <button className={classes.btn_save}>Save</button>
--- frontend/src/components/HomePageUser/index.jsx
+++ frontend/src/components/HomePageUser/index.jsx
@@ -1,9 +1,169 @@
-import React from 'react'
-import './stylemodule.css'
+import React, { useState } from 'react'
+import classes from './style.module.css'
+import Folder_dublicate_duotone from '../../assets/Folder_dublicate_duotone.svg'
+import onProgressDot from '../../assets/onProgressDot.svg'
+import doneDot from '../../assets/doneDot.svg'
+import calendar_Phong from '../../assets/calendar_Phong.svg'
+import location from '../../assets/location.svg'
+
+const document =[
+  {
+    id: 1,
+    title: "Tài liệu 1",
+    status: "On Progress",
+    location: "Tầng 1- GDH6",
+    time: "18:25 - 01/01/2024"
+  },
+  {
+    id: 2,
+    title: "Tài liệu 1",
+    status: "On Progress",
+    location: "Tầng 1- GDH6",
+    time: "18:25 - 01/01/2024"
+  },
+  {
+    id: 3,
+    title: "Tài liệu 1",
+    status: "On Progress",
+    location: "Tầng 1- GDH6",
+    time: "18:25 - 01/01/2024"
+  },
+  {
+    id: 4,
+    title: "Tài liệu 1",
+    status: "On Progress",
+    location: "Tầng 1- GDH6",
+    time: "18:25 - 01/01/2024"
+  },
+  {
+    id: 5,
+    title: "Tài liệu 1",
+    status: "On Progress",
+    location: "Tầng 1- GDH6",
+    time: "18:25 - 01/01/2024"
+  },
+  {
+    id: 6,
+    title: "Tài liệu 1",
+    status: "On Progress",
+    location: "Tầng 1- GDH6",
+    time: "18:25 - 01/01/2024"
+  },
+  {
+    id: 7,
+    title: "Tài liệu 1",
+    status: "On Progress",
+    location: "Tầng 1- GDH6",
+    time: "18:25 - 01/01/2024"
+  },
+  {
+    id: 8,
+    title: "Tài liệu 1",
+    status: "On Progress",
+    location: "Tầng 1- GDH6",
+    time: "18:25 - 01/01/2024"
+  },
+  {
+    id: 9,
+    title: "Tài liệu 1",
+    status: "On Progress",
+    location: "Tầng 1- GDH6",
+    time: "18:25 - 01/01/2024"
+  },
+  {
+    id: 10,
+    title: "Tài liệu 1",
+    status: "On Progress",
+    location: "Tầng 1- GDH6",
+    time: "18:25 - 01/01/2024"
+  },
+  {
+    id: 11,
+    title: "Tài liệu 1",
+    status: "On Progress",
+    location: "Tầng 1- GDH6",
+    time: "18:25 - 01/01/2024"
+  },
+  {
+    id: 12,
+    title: "Tài liệu 1",
+    status: "On Progress",
+    location: "Tầng 1- GDH6",
+    time: "18:25 - 01/01/2024"
+  }
+  
+]; 
 
 export const HomePageUser = () => {
+  const nameUser = "Nguyen Van A";
+  const numberHPage =200;
+  const numberPPage =1000;
+
+  const [selectHK, setSelectHK] = useState("All");
+  const handleFilter = (e) => {
+    setSelectHK(e.target.selectedOptions[0].value);
+  };
   return (
-    <div className = "nav" >HomePageUser</div>
+    <div className={classes.container}>
+      <div className={classes.info}>
+        <div className={classes.info_name}>
+          <div className={classes.info_h}> Hi, {nameUser}</div>
+          <p className={classes.info_p}>Hope you have a good day</p>
+        </div>
+        <div className={classes.info_page}>
+          <div className={classes.info_hPage}>
+            <div className={classes.hPage_img}><img src={Folder_dublicate_duotone}/></div>
+            <div className={classes.hPage_content}>
+              <div className={classes.content_title}>Số trang giấy</div>
+              <div className={classes.content_number}>{numberHPage}</div>
+            </div>
+          </div>
+          <div className={classes.info_pPage}>
+            <div className={classes.pPage_title}>Số trang đã in trong tháng</div>
+            <div className={classes.pPage_number}>{numberPPage}</div>
+          </div>
+        </div>
+      </div>
+      <div className={classes.filter}>
+          <select value={selectHK} onChange={handleFilter} className={classes.filter_HK}>
+            <option value="All">All</option>
+            <option value="HK231">HK231</option>
+            <option value="HK232">HK232</option>
+            <option value="HK233">HK233</option>
+          </select>
+          <select value={selectHK} onChange={handleFilter} className={classes.filter_calendar}>
+            <option value="All">All</option>
+            <option value="HK231">HK231</option>
+            <option value="HK232">HK232</option>
+            <option value="HK233">HK233</option>
+          </select>
+      </div>
+      <div>
+        <div className={classes.body}>
+          {document.map(item=>(
+            <div key={item.id} className={classes.document}>
+              <div className={classes.line1}>
+                <div className={classes.doc_title}>{item.title}</div>
+                {item.stats!=="Done"?(
+                  <img className={classes.doc_dot} src={onProgressDot}/>
+                ):(
+                  <img className={classes.doc_dot} src={doneDot}/>
+                )}
+                <div className={classes.doc_status}>{item.status}</div>
+              </div>
+              <div  className={classes.line2}>
+                <img className={classes.doc_locIMG} src={location} />
+                <div className={classes.doc_loc}>{item.location}</div>
+                <img className={classes.doc_calenIMG} src={calendar_Phong} />
+                <div className={classes.doc_time}>{item.time}</div>
+              </div>
+              <div className={classes.ellipsis }>. . .</div>
+            </div>
+          ))}
+      </div>
+      </div>
+      
+    </div>
 
   )
 }
